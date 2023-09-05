d699e3bc10d2c0ee2e181c72f18211c000406612
Will-H007 <88605676+Will-H007@users.noreply.github.com>
2023-09-05 18:35:07 +0800

    zsh:1: command not found: q

--- app/data_analysis.py
+++ app/data_analysis.py
@@ -1,3 +1,12 @@
 import pandas as pd
-df = pd.read_excel("data.xls")
-print(df)
+# df = pd.read_excel("data.xls")
+df = pd.read_excel("complete_movement.xls")
+
+competitiors = ['Nutrien', 'Summit', 'CBH']
+areas = [['Kwinana'], ['Albany','Bunbury','Esperance'], ['Geraldton']]
+for a in areas:
+    for p in a:
+        mask1 = df[df['From Location'].str.contains(p)]['From Location']
+        mask2 = df[df['To Location'].str.contains(p)]['To Location']
+        print(mask1)
+        print(mask2)
