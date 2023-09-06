fce0063b4dc53e0517a74ba56f0d286bcb938ee9
Dhruv8601 <dhruvjobanputra8@gmail.com>
2023-09-06 15:43:54 +0800

    bigger map

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,6 +1,7 @@
 // Get the textarea element
 const textarea = document.getElementById('inputTextarea');
 const sendButton = document.getElementById('sendButton');
+const answerTextarea = document.getElementById('answerTextarea');
 
 // Function to adjust the number of rows based on content
 function adjustTextareaRows() {
@@ -42,3 +43,11 @@ textarea.addEventListener('input', checkTextareaContent);
 
 // Initially check textarea content on page load
 checkTextareaContent();
+
+function senddata() {
+	const msg =
+		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+	if (textarea.value.trim() !== '') {
+		answerTextarea.innerText = msg;
+	}
+}
