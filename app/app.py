f3f3adbc00165edc8575ffc2e84fb72a63506e17
Will-H007 <88605676+Will-H007@users.noreply.github.com>
2023-09-06 11:57:50 +0800

    added map(incomplete)

--- app/app.py
+++ app/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/layout')
-def layout_page():
-    return render_template('layout_bubbles.html')
+@app.route('/map')
+def map_page():
+    return render_template('map.html')
+
 if __name__ == '__main__':
     app.run()
