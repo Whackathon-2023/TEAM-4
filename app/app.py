d206bfbcf82a6defa8d6b98f30669b9b007d61f8
Dhruv8601 <dhruvjobanputra8@gmail.com>
2023-09-06 11:38:09 +0800

    waves home page

--- app/app.py
+++ app/app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/layout')
 def layout_page():
-    return render_template('layout_bubbles.html')
+    return render_template('home.html')
 if __name__ == '__main__':
     app.run()
