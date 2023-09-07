fe829cc3f0b359c78bfcf05346a00f6ed44b4297
Alex Hawking <alexhawking23@gmail.com>
2023-09-07 22:04:16 +0800

    add loader

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -8,6 +8,7 @@ const data = {
 const textarea = document.getElementById('inputTextarea');
 const sendButton = document.getElementById('sendButton');
 const answerTextarea = document.getElementById('answerTextarea');
+const loader = document.getElementsByClassName('loader')[0];
 answerTextarea.style.display = 'none';
 // Function to adjust the number of rows based on content
 function adjustTextareaRows() {
@@ -66,78 +67,17 @@ checkTextareaContent();
 
 async function senddata() {
 	if (textarea.value.trim() !== '') {
+		answerTextarea.style.display = 'none';
+
+		loader.style.display = 'inline';
 
 		const result = await sendData();
 		console.log(result)
 		answerTextarea.innerText = result;
 
 		answerTextarea.style.display = 'block';
-		answerTextarea.innerHTML = result;
+		loader.style.display = 'none';
+		answerTextarea.innerHTML = result
 	}
 }
 
-/* 
-  source: https://www.codesdope.com/blog/article/12-creative-css-and-javascript-text-typing-animati/
-  modified by: Dhruv Jobanputra
-*/
-function setupTypewriter(t) {
-	let HTML = t.innerHTML;
-
-	t.innerHTML = '';
-
-	let cursorPosition = 0,
-		tag = '',
-		writingTag = false,
-		tagOpen = false,
-		typeSpeed = 20,
-		tempTypeSpeed = 0;
-
-	let type = function () {
-		if (writingTag === true) {
-			tag += HTML[cursorPosition];
-		}
-
-		if (HTML[cursorPosition] === '<') {
-			tempTypeSpeed = 0;
-			if (tagOpen) {
-				tagOpen = false;
-				writingTag = true;
-			} else {
-				tag = '';
-				tagOpen = true;
-				writingTag = true;
-				tag += HTML[cursorPosition];
-			}
-		}
-		if (!writingTag && tagOpen) {
-			tag.innerHTML += HTML[cursorPosition];
-		}
-		if (!writingTag && !tagOpen) {
-			if (HTML[cursorPosition] === ' ') {
-				tempTypeSpeed = 0;
-			} else {
-				tempTypeSpeed = Math.random() * typeSpeed + 1000 / HTML.length;
-			}
-			t.innerHTML += HTML[cursorPosition];
-		}
-		if (writingTag === true && HTML[cursorPosition] === '>') {
-			tempTypeSpeed = Math.random() * typeSpeed + 1000 / HTML.length;
-			writingTag = false;
-			if (tagOpen) {
-				let newSpan = document.createElement('span');
-				t.appendChild(newSpan);
-				newSpan.innerHTML = tag;
-				tag = newSpan.firstChild;
-			}
-		}
-
-		cursorPosition += 1;
-		if (cursorPosition < HTML.length) {
-			setTimeout(type, tempTypeSpeed);
-		}
-	};
-
-	return {
-		type: type,
-	};
-}
